322bffc22194c79a2f429dc82ef0d18b274b92b5
Arpad Kiss <arpad1337@me.com>
2017-12-12 14:13:34 +0100

    Patch

--- src/app/broadcaster.service.ts
+++ src/app/broadcaster.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Subject} from 'rxjs/Subject';
-import {Observable} from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 
@@ -19,18 +19,16 @@ export class BroadcasterService {
   }
 
   emit( key: any, data?: any ) {
-    this.eventBus.next({
+    const event = {
       key,
       data
-    });
+    };
+    console.log('Broadcaster#emit event', event);    
+    this.eventBus.next(event);
   }
 
   on<T>( key: any ): Observable<T> {
     return this.eventBus.asObservable()
-      .map((event) => {
-        console.log('Broadcaster#emit event', event);
-        return event;
-      })
       .filter((event) => event.key === key)
       .map((event) => event.data);
   }
